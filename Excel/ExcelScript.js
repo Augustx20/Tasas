6afde718d39ce0bb129e8478b384b88ad6ad7cd7
Augusto Machado <Augus1726@gmail.com>
2022-12-27 00:21:07 -0300

    Libreria xlsx

--- Excel/ExcelScript.js
+++ Excel/ExcelScript.js
@@ -1,39 +1,70 @@
+const { opts } = require('excel4node/distribution/lib/types/borderStyle');
 const ExcelJS = require('exceljs')
+const xlsx = require('xlsx')
 
-const saveExcel = (data) => {
-    const workbook = new ExcelJS.Workbook()
+var wb = xlsx.readFile('Exchange.xlsx');
 
-    const filename = 'archivo.xlsx'
+var ws = wb.Sheets['Oanda'];
 
-    const sheet = workbook.addWorksheet('resultado')
+var worksheet =  {
+    "!ref": "A1:A3",
+    A1: { t:'n', v:1 },
+    A2: { t:'n', v:2 },
+    A3: { t:'n', v:3, f:'A1+A2' }
+  };
+  xlsx.utils.sheet_set_array_formula(worksheet);
 
-    const reColumns =[
-        {header: 'BANCO UYU', key: 'UYU'},
-        {header: 'BANCO COP', key: 'COP'},
-        {header: 'BANCO CR', key: 'CR'},
-        {header: 'BANCO PERU USD', key: 'PER'},
-        {header: 'BANCO PERU EUR', key: 'PUS'},
-        {header: 'BANCO CHILE USD', key: 'CUSD'},
-        {header: 'BANCO CHILE EUR', key: 'CEUR'},
-        {header: 'CLP USD', key: 'CLPUSD'},
-        {header: 'CLP EUR', key: 'CLPEUR'},
-        {header: 'EUR COP', key: 'EURCOP'},
-        {header: 'EUR USD', key: 'EURUSD'},
-        {header: 'CNY USD', key: 'CNYUSD'},
-        {header: 'JPY USD', key: 'JPYUSD'},
-        {header: 'JPY COP', key: 'JPYCOP'},
-        {header: 'CNY COP', key: 'CNYCOP'},
-        {header: 'BRL USD', key: 'BRLUSD'},
-    ]
+xlsx.writeFile(wb,'Exchange.xlsx',opts)
 
-    sheet.columns = reColumns
-    sheet.addRows(data)
-    workbook.xlsx.writeFile(filename).then((e)=>{
-        console.log('Creado Exitosamente')
-    })
 
-}
 
-module.exports = {
-saveExcel
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const saveExcel = (data) => {
+//     const workbook = new ExcelJS.Workbook()
+
+//     const filename = 'archivo.xlsx'
+
+//     const sheet = workbook.addWorksheet('resultado')
+
+//     const reColumns =[
+//         {header: 'BANCO UYU', key: 'UYU'},
+//         {header: 'BANCO COP', key: 'COP'},
+//         {header: 'BANCO CR', key: 'CR'},
+//         {header: 'BANCO PERU USD', key: 'PER'},
+//         {header: 'BANCO PERU EUR', key: 'PUS'},
+//         {header: 'BANCO CHILE USD', key: 'CUSD'},
+//         {header: 'BANCO CHILE EUR', key: 'CEUR'},
+//         {header: 'CLP USD', key: 'CLPUSD'},
+//         {header: 'CLP EUR', key: 'CLPEUR'},
+//         {header: 'EUR COP', key: 'EURCOP'},
+//         {header: 'EUR USD', key: 'EURUSD'},
+//         {header: 'CNY USD', key: 'CNYUSD'},
+//         {header: 'JPY USD', key: 'JPYUSD'},
+//         {header: 'JPY COP', key: 'JPYCOP'},
+//         {header: 'CNY COP', key: 'CNYCOP'},
+//         {header: 'BRL USD', key: 'BRLUSD'},
+//     ]
+
+//     sheet.columns = reColumns
+//     sheet.addRows(data)
+//     workbook.xlsx.writeFile(filename).then((e)=>{
+//         console.log('Creado Exitosamente')
+//     })
+
+// }
+
+// module.exports = {
+// saveExcel
+// }
